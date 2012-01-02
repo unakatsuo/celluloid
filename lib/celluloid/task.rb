ead5dec8e2a72454e175af1800dbebb2a07a8990
Tony Arcieri <tony.arcieri@gmail.com>
2012-01-01 17:29:07 -0800

    Debugging stuff for Task that triggers Thread deadlock in ::lock()!
    I was trying to debug a problem with Celluloid and apparently ran into a
    Rubinius bug that's fairly reproducible.

--- lib/celluloid/task.rb
+++ lib/celluloid/task.rb
@@ -12,12 +12,16 @@ module Celluloid
     def self.current
       task = Thread.current[:task]
       raise "not in task scope" unless task
+      task.is_everything_ok?
       task
     end
 
     # Suspend the running task, deferring to the scheduler
     def self.suspend(value = nil)
+      Task.current.is_everything_ok?
       result = Fiber.yield(value)
+      Task.current.is_everything_ok?
+
       raise TerminatedError, "task was terminated" if result == TerminatedError
       result
     end
@@ -33,6 +37,8 @@ module Celluloid
         Thread.current[:actor]   = actor
         Thread.current[:mailbox] = mailbox
         Thread.current[:task]    = self
+
+        is_everything_ok?
 
         begin
           yield
@@ -52,6 +58,7 @@ module Celluloid
 
     # Terminate this task
     def terminate
+      is_everything_ok?
       resume TerminatedError
     rescue FiberError
       # If we're getting this the task should already be dead
@@ -63,6 +70,18 @@ module Celluloid
     # Nicer string inspect for tasks
     def inspect
       "<Celluloid::Task:0x#{object_id.to_s(16)} @type=#{@type.inspect}, @running=#{@fiber.alive?}>"
+    end
+
+    # Is all well?
+    def is_everything_ok?
+      puts "checking if everything is ok..."
+      if Task.current.instance_variable_get(:@fiber) != Fiber.current
+        puts "everything is NOT ok"
+        raise "zomgwtfbbq, Thread.current[:task] is WRONG"
+      end
+      puts "yep, everhything is ok!"
+
+      true
     end
   end
 end
